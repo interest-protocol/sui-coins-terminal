f8ad580964e314897f89d8a619998b6e3be825cb
git-marcopitra <marcomorete73@hotmail.com>
2024-10-14 00:45:24 +0100

    :green_heart: build: fix types

--- sdk/src/components/web3-manager/coins-manager/coins-manager.types.ts
+++ sdk/src/components/web3-manager/coins-manager/coins-manager.types.ts
@@ -1,7 +1,9 @@
+import { Chain } from "@interest-protocol/sui-tokens";
 import { CoinMetadata } from "@mysten/sui/client";
 import BigNumber from "bignumber.js";
 
 export interface CoinObject extends Pick<CoinMetadata, "symbol" | "decimals"> {
+  chain?: Chain;
   balance: BigNumber;
   type: `0x${string}`;
   coinObjectCount: number;
