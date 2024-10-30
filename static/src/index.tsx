5200bc6b8dc0d293a4df4430b270664a59702877
git-marcopitra <marcomorete73@hotmail.com>
2024-10-30 01:18:11 +0100

    :sparkles: feat: add test terminal

--- static/src/index.tsx
+++ static/src/index.tsx
@@ -10,7 +10,7 @@ const Terminal: FC = () => (
     slippage="5"
     typeIn={SUI_TYPE_ARG}
     aggregator={Aggregator.Hop}
-    projectAddress="0xdb3a22be6a37c340c6fd3f67a7221dfb841c818442d856f5d17726f4bcf1c8af"
+    projectAddress="0xdd224f2287f0b38693555c6077abe85fcb4aa13e355ad54bc167611896b007e6"
     typeOut="0x07ab9ba99abd9af0d687ae55079601192be5a12d1a21c8c4cd9f1a17519111e0::emoji::EMOJI"
   />
 );
