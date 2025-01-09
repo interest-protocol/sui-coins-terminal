6ec29210a4bfc9b046e1f3d51dbb850a1becae18
git-marcopitra <marcomorete73@hotmail.com>
2025-01-09 13:27:40 +0100

    :sparkles: feat: add some improvements

--- example/react/src/index.tsx
+++ example/react/src/index.tsx
@@ -6,7 +6,7 @@ const Terminal: FC = () => (
   <SwapTerminal
     slippage="5"
     typeIn={SUI_TYPE_ARG}
-    projectAddress="0xdc97a041dddf31bdf458df744430e2f5bf0bc9221bb982280e5c9c54921d0d43"
+    projectAddress="0xdd224f2287f0b38693555c6077abe85fcb4aa13e355ad54bc167611896b007e6"
     typeOut="0x07ab9ba99abd9af0d687ae55079601192be5a12d1a21c8c4cd9f1a17519111e0::emoji::EMOJI"
   />
 );
